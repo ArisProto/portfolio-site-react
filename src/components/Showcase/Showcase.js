ff2c727135ee435843cd2a24347edb587ae98fc6
misshu1 <31734905+misshu1@users.noreply.github.com>
2018-10-02 20:59:51 +0300

    fixed showcase component

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -4,8 +4,15 @@ import './Showcase.css';
 const showcase = props => (
     <div className="showcase">
         <div className="showcase-img">
-            <div className="container">
-                <p>lorem20sasf adfjb asdkjf asf asjf asf asf asf asf fas fsaf asf asf saf asf asf asf hash sj ash ado;kasdhg nlzsdfk.jlgh ksalh.g</p>
+            <div className="darken">
+                <div className="container">
+                    <div className="container-left">
+                        <h2>Alege un site care ti se potriveste.</h2>
+                    </div>
+                    <div className="container-right">
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
